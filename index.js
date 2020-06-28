500cd120f5e78d7b1309406dca92a14592c36704
Gaewoong <59592439+Gaewoong@users.noreply.github.com>
2020-06-28 21:06:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like SW!');
+    res.send('I like SII!');
 });
 
 app.get('/start', function(req, res){
